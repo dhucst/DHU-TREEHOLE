8c2e8083a7d820c69d94ccc4d257dcbd03ae2516
Cyprinus <Cyprinus12138@gmail.com>
2019-02-04 10:27:16 +0800

    Update signup
    Add validator of the stdId & email.

--- backend/models/users.js
+++ backend/models/users.js
@@ -3,4 +3,26 @@ const UserSchema = require('../lib/database').UserSchema;
 
 const UserModel = mongoose.model('User', UserSchema);
 
+function ifStdIdValid(stdId, callback) {
+  UserModel.findOne({
+    stdId,
+  }, (err, user) => {
+    callback(null, !user);
+  });
+}
+
+function ifStdIdEmailValid(stdId, Email, callback) {
+  ifStdIdValid(stdId, (err, flag) => {
+    if (!flag) callback(null, false);
+    else {
+      UserModel.findOne({
+        email: Email.toLocaleLowerCase(),
+      }, (err, user) => {
+        callback(null, !user);
+      });
+    }
+  });
+}
+
 exports.User = UserModel;
+exports.ifStdIdEmailValid = ifStdIdEmailValid;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const User = require('../models/users').User;
+const ifStdIdEmailValid = require('../models/users').ifStdIdEmailValid;
 
 const router = express.Router();
 
 
 router.post('/signup', (req, res) => {
-  const user = new User({
-    stdId: req.body.stdId,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  user.save((err) => {
+  ifStdIdEmailValid(req.body.stdId, req.body.email.toLocaleLowerCase(), (err, flag) => {
     if (err) {
       res.status(500);
       res.json({
@@ -19,20 +16,57 @@ router.post('/signup', (req, res) => {
         msg: 'Failed.',
         token: null,
       });
+      return;
     }
-    const tmp = {
-      _id: user._id,
-      stdId: user.stdId,
-    };
-    jwt.sign(tmp, process.env.superSecret, {
-      expiresIn: 60 * 60 * 24,
-    }, (err, token) => {
+    if (!flag) {
+      res.status(403);
       res.json({
-        success: true,
-        msg: 'success',
-        token,
+        success: false,
+        msg: '学号/邮箱非法或已注册',
+        token: null,
       });
-    });
+    } else {
+      bcrypt.hash(req.body.password, parseInt(process.env.saltRounds, 10), (err, encoded) => {
+        if (err) {
+          res.status(500);
+          res.json({
+            success: false,
+            msg: 'Failed.',
+            token: null,
+          });
+          return;
+        }
+        const user = new User({
+          stdId: req.body.stdId,
+          email: req.body.email.toLocaleLowerCase(),
+          password: encoded,
+        });
+        user.save((err) => {
+          if (err) {
+            res.status(500);
+            res.json({
+              success: false,
+              msg: 'Failed.',
+              token: null,
+            });
+            return;
+          }
+          const tmp = {
+            _id: user._id,
+            stdId: user.stdId,
+          };
+          jwt.sign(tmp, process.env.superSecret, {
+            expiresIn: 60 * 60 * 24,
+          }, (err, token) => {
+            res.json({
+              success: true,
+              msg: 'success',
+              token,
+            });
+          });
+        });
+      });
+    }
   });
 });
 
