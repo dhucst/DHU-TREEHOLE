823b9317e79ecd616e2f43087f942d9e92f90d44
Cyprinus <Cyprinus12138@gmail.com>
2019-02-14 23:01:53 +0800

    Update PostModel.
    Add getAbstractById method.

--- backend/models/posts.js
+++ backend/models/posts.js
@@ -3,4 +3,27 @@ const PostSchema = require('../lib/database').PostSchema;
 
 const PostModel = mongoose.model('Post', PostSchema);
 
+function getAbstractById(postId, callback) {
+  PostModel.findById(postId, (err, post) => {
+    if (err || !post) {
+      callback(err ? err : null, null);
+      return;
+    }
+    const tmp = {
+      owner: post.owner,
+      createTime: post.createTime,
+      content: post.content,
+      pictures: post.pictures.length ? post.pictures[0] : null,
+      approves: post.approves.length >= 5 ? post.approves.slice(0,5) : post.approves,
+      approveNum: post.approves.length,
+      commentNum: post.comments.length,
+      background: post.background,
+      isAnonymous: post.isAnonymous,
+    };
+    callback(null, tmp)
+  });
+}
+
+PostModel.getAbstractById = getAbstractById;
+
 exports.Post = PostModel;
